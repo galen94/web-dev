0e708c5cc61ae5aa1337ace89ec8a401395669c6
Galen Otten <galen.louise@gmail.com>
2022-02-26 17:54:52 -0500

    fixing a5

--- public/tuiter/PostSummaryList/PostSummaryItem.js
+++ public/tuiter/PostSummaryList/PostSummaryItem.js
@@ -1,12 +1,16 @@
+
+
 const PostSummaryItem = (post) => {
     return(`
            <li class=" list-group-item ">
             <div id="append-new-elements">
                 <img class="wd-post-image wd-rounded-corners-all-around" src=${post.image} />
 
-                ${post.booleantopic ? <span class='wd-gray-words wd-margin-left'> ${post.topic}</span>: ""}
-<!--                 <span class="wd-gray-words wd-margin-left "> {post.topic}</span>-->
-<!--                 <br>-->
+<!--                {post.topic ? <span class='wd-gray-words wd-margin-left'> {post.booleantopic}</span>: ""}-->
+                 <span class="wd-gray-words wd-margin-left "> ${post.topic}</span>
+                 <br>
+
+
                 <span class="wd-white-words wd-margin-left "> ${post.userName}</span>
                 <i class="wd-white-circle fa fa-circle"></i>
                 <span class="wd-gray-words"> - ${post.time}</span>
