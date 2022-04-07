93b5ec8c29d4fd125ce858700543dd4f9256e1ba
Galen Otten <galen.louise@gmail.com>
2022-04-07 10:57:44 -0400

    adding missing semicolon

--- src/services/tuits-service.js
+++ src/services/tuits-service.js
@@ -1,4 +1,4 @@
-import axios from 'axios'
+import axios from 'axios';
 //const TUITS_API = 'http://localhost:4000/api/tuits';
 //const TUITS_API = 'https://otten-web-dev-server-node.herokuapp.com/api/tuits';
  const API_BASE = process.env.REACT_APP_API_BASE;
